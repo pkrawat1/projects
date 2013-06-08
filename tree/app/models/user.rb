2423e6d8fcdd80c35016c835ab104c4ed62fb9c9
pkrawat1 <pankajrawat19sept@gmail.com>
2013-06-08 16:12:16 +0530

    test included

--- tree/app/models/user.rb
+++ tree/app/models/user.rb
@@ -13,9 +13,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name,presence: true
+validates :last_name,presence: true
+validates :profile_name,presence: true,
+                        uniqueness: true,
+                        format:{
+                          with: /a-zA-Z0-9_-/,
+                          message: 'Must be formatted correctly'
+                        }
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name,:last_name,:profile_name,:email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  has_many :statuses
   def full_name
     first_name + " " + last_name
   end
